d5a0e55a59b1015a3dfa4146c953a19f4b31135f
Alona Dmytrokhina <dmitrohina1708@gmail.com>
2024-06-20 22:33:46 +0300

    Added report

---
+++ report.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const savedCart = localStorage.getItem('cart');
+    let cartData = [];
+
+    if (savedCart) {
+        const cart = JSON.parse(savedCart);
+        Object.keys(cart).forEach(key => {
+            const item = cart[key];
+            cartData.push({
+                "Піца": item.pizza.title,
+                "Розмір": item.size === 'small' ? 'Мала' : 'Велика',
+                "Ціна": item.size === 'small' ? item.pizza.small_size.price : item.pizza.big_size.price,
+                "Кількість": item.quantity
+            });
+        });
+    }
+
+    new WebDataRocks({
+        container: "#report-container",
+        toolbar: true,
+        report: {
+            dataSource: {
+                data: cartData
+            },
+            slice: {
+                rows: [
+                    { uniqueName: "Піца" },
+                    { uniqueName: "Розмір" }
+                ],
+                measures: [
+                    { uniqueName: "Кількість"},
+                    { uniqueName: "Ціна"}
+                ]
+            }
+        }
+    });
+});
